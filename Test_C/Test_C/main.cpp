fd5dd557335828e684a9f57094adbea081056eb1
Jinwoo Kim <kidjinwoo@me.com>
2020-01-07 18:40:24 -0800

    Upload Test_C

---
+++ Test_C/Test_C/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Test_C
+//
+//  Created by pook on 1/7/20.
+//  Copyright © 2020 pookjw. All rights reserved.
+//
+
+#include <iostream>
+
+void say_hello(){
+    printf("Hello!\n");
+}
+
+int main(int argc, const char * argv[]) {
+    printf("Hello World!\n");
+    say_hello();
+    return 0;
+}
